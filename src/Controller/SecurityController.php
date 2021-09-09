07f5f9a1b84a7d38f4e74d76098d20649ea320d9
corentinthibaud <corentin.thibaud@isen.yncrea.fr>
2021-09-09 21:01:29 +0200

    Adding tailwind and security

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -27,7 +27,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
-             return $this->redirectToRoute('app_home');
+             return $this->redirectToRoute('home');
         }
 
         // get the login error if there is one
@@ -41,9 +41,9 @@ class SecurityController extends AbstractController
     /**
      * @Route("/logout", name="app_logout")
      */
-    public function logout()
+    public function logout() : LogicException
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        throw new LogicException('Methode qui peut être nulle');
     }
 
     /**
