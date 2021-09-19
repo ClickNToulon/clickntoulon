9f6e92b7601322f9cba0662a8227bdec486b797d
corentinthibaud <corentin.thibaud@isen.yncrea.fr>
2021-09-19 22:58:54 +0200

    Correcting migrations and bugs

--- migrations/Version20210919205702.php
+++ migrations/Version20210919205702.php
@@ -10,22 +10,22 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210416092941 extends AbstractMigration
+final class Version20210919205702 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product DROP test');
+
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE product ADD test VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_unicode_ci`');
+
     }
 }
